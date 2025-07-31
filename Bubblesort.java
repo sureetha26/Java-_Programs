496f914ecbc909921942bc9ca809ee04e4ba6c98
Sureetha <surepallesureetha29@gmail.com>
2025-07-31 10:41:05 +0530

    Update Bubblesort.java

--- Bubblesort.java
+++ Bubblesort.java
@@ -7,7 +7,6 @@ public class Bubblesort {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n - 1 - i; j++) {
                 if (arr[j] > arr[j + 1]) {
-                    // Swap adjacent elements
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
